525dcaf7c0bb535fbf0f6daee0c3e1baa6a5c7a7
Saied <elsaied.samaka1997@gmail.com>
2024-04-04 04:43:35 +0200

    full setup

--- core/store/configureStore.js
+++ core/store/configureStore.js
@@ -1,14 +1,20 @@
-// redux/store.js
-
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
 import rootSaga from "../sagas";
+import sagaMonitor from "@redux-saga/simple-saga-monitor";
+import createLogger from "redux-logger";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware, createLogger)
+);
 
 sagaMiddleware.run(rootSaga);
+
+store.runSaga = sagaMiddleware.run;
+store.close = () => store.dispatch(END);
 
 export default store;
